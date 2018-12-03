ba47c5ea43974a03e0c7c056b3d277160e15557d
Stephen Weiss <stephencweiss@gmail.com>
2018-12-03 14:41:05 -0800

    Budget page renders
    The visualize has a placeholder clickHandler.
    The budget component is commented until it is scoped and exports something that can be imported.

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import axios from 'axios';
 
+import Budget from './budget.jsx'
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       user:'',
       budgetName:'',
+      budget: [],
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick (event) {
+    console.log(`Alert! You've selected visualize`);
   }
 
   render() {
     return (
       <div>
-      <h1>Welcome to your time budget!</h1>
-      <div id="username">
-        <h2 >User: {this.state.user}</h2>
-      </div>
-      <div id="budgetName">
-        <h2>Budget: {this.state.budgetName}</h2>
-      </div>
+        <h1>Welcome to your time budget!</h1>
+        <div id="username">
+          <h2 >User: { this.state.user }</h2>
+        </div>
+        <div id="budgetName">
+          {/* Confirm budgetName is established *prior* to loading page */}
+          {/* It _may_ come through with the onMount call */}
+          <h2>Budget: { this.state.budgetName }</h2>
+        </div>
+        {/* <div id="budget">
+          <Budget
+            budget = { this.state.budget }
+          />
+        </div> */}
+        <button id="visualizeBudget" className="button" onClick={ this.handleClick }>Visualize</button>
       </div>
     );
   }
